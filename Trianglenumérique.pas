07424a9cb26c136b1e155e75ba985bb578d6ad92
XELO-PC\julie <j.lirot@ludus-academie.com>
2017-01-24 17:54:54 +0100

    ALGO_TP1_TD3_LirotJulien

---
+++ Trianglenumérique.pas
@@ -0,0 +1,41 @@
+﻿program trianglenumérique;
+
+uses crt;
+
+(*
+//BUT : Fait un triangle en numéros
+//ENTREE : La taille du triangle en numéros
+//SORTIE : Un triangle en numéros de la taille désirée
+
+VAR
+	taille, i, j : ENTIER
+	
+DEBUT
+	ECRIRE 'Entrez la taille du triangle en numéros !'		
+	LIRE taille									//Récupération de la taille du triangle en numéros
+	POUR i DE 1 A taille FAIRE					//Boucle pour ecrire les lignes (diminue la longueur de la ligne à chaque ligne)
+		POUR j de 1 A (taille - i + 1) FAIRE	//Boucle qui écrit le contenu de la ligne (diminue la longueur de la ligne à chaque ligne)
+			ECRIRE(i-1)							//Le chiffre écrit est i - 1 pour commencer à 0 au lieu de 1
+		FINPOUR
+	FINPOUR
+FIN
+*)
+
+
+VAR	
+	taille, i, j : INTEGER;
+	
+BEGIN
+	clrscr;
+	writeln('Entrez la taille du triangle en numéros !');
+	readln(taille);												//récupération de la taille
+	FOR i := 1 TO taille DO										//Boucle pour écrire les lignes.
+	BEGIN
+		FOR j := 1 TO (taille -i + 1) DO						//Boucle qui écrit le contenu de la ligne définie par la boucle précédente.
+		BEGIN
+			write(i-1);											//Le chiffre écrit est i - 1 pour commencer à 0 au lieu de 1
+		END;
+	writeln();
+	END;
+	readln();
+END.
---
+++ triangleXO.pas
@@ -0,0 +1,56 @@
+﻿program triangleXO;
+
+uses crt;
+
+{
+//BUT : Créer un triangle en X et O
+//ENTREE : La taille du triangle en X et O
+//SORTIE : un triangle en X et O de la taille désirée
+
+VAR
+	i, j, taille : ENTIER
+	
+DEBUT
+	ECRIRE La taille du triangle ?
+	LIRE taille
+	POUR i DE 1 A taille - 1 FAIRE
+	DEBUT
+		POUR j DE 1 A taille FAIRE
+			SI ((j = 1) OU (j = i)) ALORS
+				ECRIRE('X')
+			SINON
+				SI j < i ALORS ECRIRE('O')
+			FINSI
+		FINPOUR
+		ECRIRE()
+	FINPOUR
+	POUR i DE 1 A taille FAIRE 
+	ECRIRE('X')
+FIN
+}
+
+
+VAR
+	i, j, taille : INTEGER;
+	
+BEGIN
+	clrscr;
+	writeln('La taille du triangle ?');
+	readln(taille);								//Demande à l'utilisateur la taille du futur triangle
+	FOR i := 1 TO (taille - 1) DO
+	BEGIN										//Boucles qui permettront de savoir ou poser les X et les O sauf pour la dernière ligne
+		FOR j := 1 TO taille DO					
+		BEGIN
+			IF ((j = 1) OR (j = i)) THEN		//Si le caractère écrit est le premier ou le dernier de la ligne il sera un X
+				write('X')
+			ELSE
+			BEGIN
+				IF j < i THEN write('O');		//Si la caractère écrit est entre le premier et le dernier il sera un O
+			END;
+		END;
+		writeln();								//Passage à la ligne suivante
+	END;
+	FOR i := 1 TO taille DO						//Boucle pour écrire la dernière ligne qui ne sera pas traité par les deux premières boucles
+	write('X');
+	readln();
+END.
